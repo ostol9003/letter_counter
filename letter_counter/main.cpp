b0fc996a6deb73484680c38703f72795ec72ade4
OstolskiM <ostolski.marcin@gmail.com>
2022-02-13 19:43:21 +0100

    ready

--- letter_counter/main.cpp
+++ letter_counter/main.cpp
@@ -6,9 +6,56 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+void welcome()
+{
+    cout<<"\t\tHello!\nI will count letters!\n\n";
+};
+
+
+int main(int argc, const char * argv[])
+{
+    int s;
+    int count[123]={};
+    
+    welcome();                          //welcome function
+    
+    cout<<"\nEnter quantity of sentences: ";
+    cin>>s;
+    
+    char tab[s][150];
+    
+    cin.ignore();
+    for (int i=0;i<s;i++)
+    {
+        cout<<"\nEnter sentence number "<<i+1<<": "<<endl;
+        cin.getline(tab[i],123);        //line loading
+       
+    }
+    cout<<"\nYours sentences:\n";
+    for(int i=0;i<s;i++)
+        cout<<tab[i]<<endl;
+    cout<<"\n\nLetters and their quantity:\n";
+    for(int i=0;i<s;i++)
+    {
+        int y=0;                        //variable for moving through letters
+        while(tab[i][y])
+        {
+            ++count[tab[i][y]];         //counting characters
+            y++;
+        }
+    }
+    
+    for(int i=65;i<91;i++)              // big letters
+        if(count[i]>0)
+            cout<<(char)i<<" - "<<count[i]<<endl;
+    
+    for(int i=97;i<123;i++)             //small letters
+        if(count[i]>0)
+            cout<<(char)i<<" - "<<count[i]<<endl;
+    
+  
+    
 }
